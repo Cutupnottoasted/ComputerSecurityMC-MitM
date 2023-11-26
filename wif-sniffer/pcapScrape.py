32132fb49cde7066ec3c676f088568a2999a51a5
cnh71 <cnh71@txstate.edu>
2023-11-26 15:44:43 -0600

    debugger and info logger
    moved pcap files into data directory and added logging functions. All outputs now lead to .log file and is reset every run

--- wif-sniffer/pcapScrape.py
+++ wif-sniffer/pcapScrape.py
@@ -34,24 +34,31 @@ def process_pcap(pcap_file, block_traffic=False):
                 print(f"\nPacket {packet_number} flagged as potential attacker - Reason: {reason}")
                 if block_traffic:
                     print(f"Blocking traffic for this packet.")
+                    print(f"User dropped: {src_mac}")
                     return  # Exit the function, simulating blocking traffic
         else:
             src_ip = packet[IP].src if IP in packet else "N/A"
             dst_ip = packet[IP].dst if IP in packet else "N/A"
-            src_port = packet[IP].sport if IP in packet else "N/A"
-            dst_port = packet[IP].dport if IP in packet else "N/A"
+            src_port = packet.sport if packet.haslayer(IP) and packet.haslayer(TCP) else "N/A"
+            dst_port = packet.dport if packet.haslayer(IP) and packet.haslayer(TCP) else "N/A"
             protocol = packet[IP].proto if IP in packet else "N/A"
 
-            # Print the entire packet details for non-Dot11 packets
-            print(f"\nPacket {packet_number} - Timestamp: {formatted_time} - Length: {len(packet)} bytes")
-            print(f"Source IP: {src_ip}, Destination IP: {dst_ip}")
-            print(f"Source Port: {src_port}, Destination Port: {dst_port}, Protocol: {protocol}")
-
-            if block_traffic:
+            if not any(data["count"] > 0 for data in attackers.values()):
+                # Print the entire packet details for non-Dot11 packets
+                print(f"\nPacket {packet_number} - Timestamp: {formatted_time} - Length: {len(packet)} bytes")
+                print(f"Source IP: {src_ip}, Destination IP: {dst_ip}")
+                print(f"Source Port: {src_port}, Destination Port: {dst_port}, Protocol: {protocol}")
+            elif block_traffic:
                 reason = "Handshake failed"  # Simulated reason for blocking
-                print(f"Packet {packet_number} flagged as potential attacker - Reason: {reason}")
+                print(f"\nPacket {packet_number} flagged as potential attacker - Reason: {reason}")
                 print(f"Blocking {src_ip}:{src_port} - Reason: {reason}")
-                return  # Exit the function, simulating blocking traffic
+                print(f"User dropped: {src_ip}")
+            else:
+                # Print details of potential attacker packets
+                if any(data["count"] > 0 for data in attackers.values()):
+                    print(f"\nPacket {packet_number} - Potential Attacker Details:")
+                    print(f"Source IP: {src_ip}, Destination IP: {dst_ip}")
+                    print(f"Source Port: {src_port}, Destination Port: {dst_port}, Protocol: {protocol}")
 
     for packet_number, packet in enumerate(packets, 1):
         process_packet(packet, packet_number)
@@ -64,8 +71,8 @@ def process_pcap(pcap_file, block_traffic=False):
     return attackers
 
 # Specify the names of your PCAP files
-pcap_file1 = "example-tptk-attack.pcapng"
-pcap_file2 = "nf9-juniper-vmx.pcapng.cap"
+pcap_file1 = "teardrop.cap"
+pcap_file2 = "example-tptk-attack.pcapng"
 
 # Process the first pcap file
 attackers1 = process_pcap(pcap_file1, block_traffic=True)
@@ -80,11 +87,15 @@ threshold = 1
 # Print or report potential attackers for the first file
 if any(data["count"] > threshold for data in attackers1.values()):
     print(f"\nPotential Attackers detected in {pcap_file1}")
-else:
-    print(f"\nNo Potential Attackers detected in {pcap_file1}")
+    for mac, data in attackers1.items():
+        if data["count"] > threshold:
+            print(f"  Source MAC: {mac} (Packets: {data['count']})")
+            print(f"  Blocked: {mac}\n")
 
 # Print or report potential attackers for the second file
 if any(data["count"] > threshold for data in attackers2.values()):
     print(f"\nPotential Attackers detected in {pcap_file2}")
-else:
-    print(f"\nNo Potential Attackers detected in {pcap_file2}")
+    for mac, data in attackers2.items():
+        if data["count"] > threshold:
+            print(f"  Source MAC: {mac} (Packets: {data['count']})")
+            print(f"  Blocked: {mac}\n")
